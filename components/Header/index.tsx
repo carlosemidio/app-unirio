702955612a3092055e2cf2c3b66122f74c2df235
Carlos <carlos@MacBook-Air-de-Carlos.local>
2021-07-29 08:14:05 -0300

    Logo

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -3,13 +3,13 @@ import { Container } from '@material-ui/core';
 
 import styles from "./styles.module.scss";
 import Image from 'next/image';
-import logoPic from '../public/logo.png'
+import logoPic from '/public/logo.png'
 
 const Header: React.FC = () => {
     return (
         <header className={styles.header}>
             <Container className={styles.headerContainer}>
-                <Image src={logoPic} alt="Logo" /> <h2>Accountability Evaluation Diagram</h2>
+                <Image src={logoPic} alt="Logo" height={70} width={250} /> <h2>Accountability Evaluation Diagram</h2>
             </Container>
         </header>
     );
